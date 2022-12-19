8603ce5f59ac31c8b519f51065800011388d5697
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-18 21:36:47 -0800

    In reducers folder, index.js and productReducer.js files are created

--- src/redux/actions/productsActions.js
+++ src/redux/actions/productsActions.js
@@ -1,19 +1,21 @@
-import { ActionTypes } from "../constants/action-types";
+import { ActionTypes } from '../constants/action-types';
 
 export const setProducts = (products) => {
   return {
     type: ActionTypes.SET_PRODUCTS,
-    payload: products,
+    payload: products
   };
 };
 
 export const selectedProduct = (product) => {
   return {
     type: ActionTypes.SELECTED_PRODUCT,
-    payload: product,
+    payload: product
   };
 };
-export const removeSelectedProduct = () => {
-  return {
-    type: ActionTypes.REMOVE_SELECTED_PRODUCT,
-  };
+
+// export const removeSelectedProduct = () => {
+//   return {
+//     type: ActionTypes.REMOVE_SELECTED_PRODUCT
+//   };
+// };
