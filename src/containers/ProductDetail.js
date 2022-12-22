d00f61b27b9791e33632bec75e8d666d0350ac13
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-22 11:32:51 -0800

    In ProductComponent.js, Link from react-router-dom added. In ProductDetail.js, useEffect hook, axios, and useParams from react-router-dom added to define productId equals useParams

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const ProductDetail = () => {
+  const { productId } = useParams();
+  console.log(productId);
   return (
     <div>
       <h1>ProductDetail</h1>
