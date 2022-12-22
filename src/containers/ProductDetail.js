2f44895ad0f4ed6a7d473bc3ede9468c0d9a8e4d
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-22 12:36:20 -0800

    In productReducer.js, selectedProductReducer is added as well it is imported into index.js as product: selectedproductReducer

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectedProduct } from '../redux/actions/productsActions';
 
 const ProductDetail = () => {
+  const product = useSelector((state) => state.product);
   const { productId } = useParams();
   const dispatch = useDispatch();
 
-  console.log(productId);
+  console.log(product);
 
   const fetchProductDetail = async () => {
     const response = await axios
